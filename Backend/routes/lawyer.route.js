2e8d82c17520756838c6925c636557525184c925
deshanFdo <62216403+deshanFdo@users.noreply.github.com>
2025-03-08 12:26:16 +0530

    Delete Backend/routes/lawyer.route.js

--- Backend/routes/lawyer.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import lawyerAuth from "../middleware/lawyerAuth.js";
-import { getLawyerData } from "../controllers/lawyer.controller.js";
-
-const lawyerRouter = express.Router();
-
-lawyerRouter.get('/data', lawyerAuth, getLawyerData);
-
-export default lawyerRouter;
