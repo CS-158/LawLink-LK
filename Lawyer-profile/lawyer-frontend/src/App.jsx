e23ee93fd45a1d19ed508a1be89de91844739945
Ranindi <ranindisamithya@gmail.com>
2025-03-11 08:34:32 +0530

    update

---
+++ Lawyer-profile/lawyer-frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import ProfileSettings from './components/ProfileSettings';
+
+const App = () => {
+  const [profileData, setProfileData] = useState({
+    profilePicture: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    displayName: "John Doe",
+    practiceAreas: "Criminal Law, Corporate Law",
+  });
+
+  const handleSaveProfile = (updatedData) => {
+    setProfileData(updatedData);
+  };
+
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header 
+          displayName={profileData.displayName} 
+          practiceAreas={profileData.practiceAreas} 
+          profilePicture={profileData.profilePicture} 
+        />
+        <ProfileSettings 
+          profilePicture={profileData.profilePicture}
+          displayName={profileData.displayName}
+          practiceAreas={profileData.practiceAreas}
+          onSave={handleSaveProfile}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
