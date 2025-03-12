69b46a254e6c6280f4f5c3b70a19fab57997b751
dtssirimanne <dtssirimanne225@gmail.com>
2025-03-12 09:58:39 +0530

    updated

---
+++ case-posting/Backend/routes/posts.js
@@ -0,0 +1,27 @@
+import express from "express";
+import Post from "../models/Post.js";
+
+const router = express.Router();
+
+// Create a new post
+router.post("/", async (req, res) => {
+  try {
+    const post = new Post(req.body);
+    await post.save();
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get all posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
